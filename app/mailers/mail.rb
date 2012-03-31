2b5da50eacb8c0e21af521bd559591fada1db095
Brian Hildebrandt <freebigb@hotmail.com>
2012-03-30 20:27:37 -0400

    Changed email settings

--- app/mailers/mail.rb
+++
@@ -1,9 +0,0 @@
-class Mail &lt; ActionMailer::Base
-
-  default :from => "admin@broadenandbuild.com"
-  # send a signup email to the user, pass in the user object that contains the user's email address
-  def registration_confirmation(user)
-    mail( :to => user.email,
-    :subject => "Thanks for signing up" )
-  end
-end
