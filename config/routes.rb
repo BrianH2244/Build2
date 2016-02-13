044605a13be162237d89975e305440ef2c74e6ff
Brian Hildebrandt <brian_hildebrandt@hotmail.com>
2016-02-13 11:29:30 -0500

    Refactored

--- config/routes.rb
+++ config/routes.rb
@@ -125,3 +125,4 @@ Build::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
 end
+
